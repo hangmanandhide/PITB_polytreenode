753ad0ef28884e2eac6722756d8623c3e99a2b7b
hangmanandhide <9014584+hangmanandhide@users.noreply.github.com>
2019-07-19 15:18:34 -0700

    Setup initialize and parent= methods for PolyTreeNode class

--- lib/00_tree_node.rb
+++ lib/00_tree_node.rb
@@ -1,3 +1,23 @@
 class PolyTreeNode
 
+  attr_accessor :children, :parent, :value
+
+  def initialize(value, parent = nil)
+    @parent = parent
+    @children = []
+    @value = value
+  end
+
+  def parent=(parent)
+    @parent = parent
+    if parent != nil && !parent.children.include?(self)
+      parent.children << self
+    end
+  end
+
+
+  def inspect
+    { "parent" => @parent, "children" => @children, "value" => @value }.inspect
+  end
+
 end
