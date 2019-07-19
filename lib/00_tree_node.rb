b1695afd9f429fe92eeb7ce778e0c8262596f018
hangmanandhide <9014584+hangmanandhide@users.noreply.github.com>
2019-07-19 15:45:35 -0700

    Fixed parent= method to properly handle new/old parent and children exchange

--- lib/00_tree_node.rb
+++ lib/00_tree_node.rb
@@ -9,12 +9,20 @@ class PolyTreeNode
   end
 
   def parent=(parent)
+    old_parent = @parent
+    
+    if old_parent != nil
+      old_parent.children.delete_if {|child| child == self }
+    end
+
     @parent = parent
     if parent != nil && !parent.children.include?(self)
       parent.children << self
     end
+
   end
 
+  #In addition to (1) re-assigning the parent attribute of the child and (2) adding it to the new parent's array of children, we should also remove the child from the old parent's list of children (if the old parent isn't nil). Modify your #parent= method to do this.
 
   def inspect
     { "parent" => @parent, "children" => @children, "value" => @value }.inspect
